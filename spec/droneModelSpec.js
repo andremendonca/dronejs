1b30f23fbaa045c57aea6df23c007be738d973f7
Andre Mendonça <albarbosa@sp.r7.com>
2012-04-24 20:41:40 -0300

    Base class become private, but we maintained Drone.Base as a deprecated alias. We intend to remove it in the future.

--- spec/droneModelSpec.js
+++ spec/droneModelSpec.js
@@ -1,10 +1,5 @@
 describe("Drone.Model", function () {
   it("should be an alias to Drone.Base", function () {
-    var modelMock = {attr: "drone"};
-    spyOn(Drone, "Base").andReturn("drone base");
-
-    var modelInstance = Drone.Model(modelMock);
-    expect(modelInstance).toEqual("drone base");
-    expect(Drone.Base).toHaveBeenCalledWith(modelMock);
+    expect(Drone.Model).toEqual(Drone.Base);
   });
 });
